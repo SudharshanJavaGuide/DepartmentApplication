54dfca74c404723a8a5d35a21c37faabffdad025
Sudharshan <sudharshanjavaguide@gmail.com>
2021-11-08 11:56:48 +0530

    "Constructor Added In College Class"

--- DepartmentApplication/src/com/sudharshanit/College.java
+++ DepartmentApplication/src/com/sudharshanit/College.java
@@ -5,5 +5,8 @@ public class College {
 	private Integer collegeId;
 	private String collegeName;
 	
-	
+	public College(Integer collegeId,String collegeName) {
+		this.collegeId=collegeId;
+		this.collegeName=collegeName;
+	}
 }
